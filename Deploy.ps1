c2bc581edae00f53d625d9d1729e8aba699cc2ba
Brian Mantay <brian.mantay@readify.net>
2017-06-26 17:08:50 +1000

    Script to deploy my ARM template. Currently depends on an existing 'docker' resource group and a KeyVault

---
+++ Deploy.ps1
@@ -0,0 +1,9 @@
+﻿$resourceGroup = "docker"
+
+Login-AzureRMAccount
+
+New-AzureRmResourceGroup -Name $resourceGroup -Location "eastus"
+
+New-AzureRmResourceGroupDeployment -Name DockerHost -ResourceGroupName $resourceGroup `
+  -TemplateUri https://raw.githubusercontent.com/Brianmantay/cicd-pipe/master/tc-docker-host/azuredeploy.json `
+  -TemplateParameterUri https://raw.githubusercontent.com/Brianmantay/cicd-pipe/master/tc-docker-host/azuredeploy.parameters.json
