adc1847c11d5c07191d167e03aa774309246f716
Brian Mantay <brian.mantay@readify.net>
2017-06-27 17:27:42 +1000

    push timestamp through to ensure custom script is always executed

--- Deploy.ps1
+++ Deploy.ps1
@@ -1,9 +1,16 @@
 ﻿$resourceGroup = "docker"
+$timestamp = [int](Get-Date -UFormat %s -Millisecond 0)
 
-Login-AzureRMAccount
+if (!$AzureRMAccount.Context.Tenant) {
+    $AzureRMAccount = Add-AzureRmAccount 
+}
 
 New-AzureRmResourceGroup -Name $resourceGroup -Location "eastus"
 
 New-AzureRmResourceGroupDeployment -Name DockerHost -ResourceGroupName $resourceGroup `
-  -TemplateUri https://raw.githubusercontent.com/Brianmantay/cicd-pipe/master/tc-docker-host/azuredeploy.json `
-  -TemplateParameterUri https://raw.githubusercontent.com/Brianmantay/cicd-pipe/master/tc-docker-host/azuredeploy.parameters.json
+    -TemplateUri https://raw.githubusercontent.com/Brianmantay/cicd-pipe/master/tc-docker-host/azuredeploy.json `
+    -TemplateParameterUri https://raw.githubusercontent.com/Brianmantay/cicd-pipe/master/tc-docker-host/azuredeploy.parameters.json
+    -timestamp $timestamp
+
+
+   
