ffac5f70c217eb2fd2d0d1c4ed957742019955b1
Brian Mantay <brian.mantay@readify.net>
2017-06-27 17:16:50 +1000

    sudo for permissions error during deployment

--- tc-docker-host/setup.sh
+++ tc-docker-host/setup.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # create host directories for volumes
-mkdir /home/bmantay/tc/data -p && mkdir /home/bmantay/tc/logs -p && mkdir /home/bmantay/tc/conf -p
+sudo mkdir /home/bmantay/tc/data -p && mkdir /home/bmantay/tc/logs -p && mkdir /home/bmantay/tc/conf -p
 
 # run server container
-docker run -d --name teamcity-server-instance  \
+sudo docker run -d --name teamcity-server-instance  \
 	-v /home/bmantay/tc/data:/data/teamcity_server/datadir \
 	-v /home/bmantay/tc/logs:/opt/teamcity/logs  \
 	-p 8111:8111 \
 	jetbrains/teamcity-server
 
 # run agent container	
-docker run -d -e SERVER_URL="dockerhost-bmantay.eastus.cloudapp.azure.com:8111" \
+sudo docker run -d -e SERVER_URL="dockerhost-bmantay.eastus.cloudapp.azure.com:8111" \
 	--name teamcity-agent-instance \
 	-v /home/bmantay/tc/conf:/data/teamcity_agent/conf \
 	jetbrains/teamcity-agent
