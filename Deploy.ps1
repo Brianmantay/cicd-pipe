3ecb5f535349c0b7cf6f042daef0187cbeb7d172
Brian Mantay <brian.mantay@readify.net>
2017-06-28 19:46:06 +1000

    fix dependency reference between docker and custom script extensions

--- Deploy.ps1
+++ Deploy.ps1
@@ -9,7 +9,7 @@ New-AzureRmResourceGroup -Name $resourceGroup -Location "eastus"
 
 New-AzureRmResourceGroupDeployment -Name DockerHost -ResourceGroupName $resourceGroup `
     -TemplateUri https://raw.githubusercontent.com/Brianmantay/cicd-pipe/master/tc-docker-host/azuredeploy.json `
-    -TemplateParameterUri https://raw.githubusercontent.com/Brianmantay/cicd-pipe/master/tc-docker-host/azuredeploy.parameters.json
+    -TemplateParameterUri https://raw.githubusercontent.com/Brianmantay/cicd-pipe/master/tc-docker-host/azuredeploy.parameters.json `
     -timestamp $timestamp
 
 
