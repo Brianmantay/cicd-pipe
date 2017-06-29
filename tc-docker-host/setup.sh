6a3d1b23de3b9edf98588975d661741a31c880c4
Brian Mantay <brian.mantay@readify.net>
2017-06-29 11:25:39 +1000

    move the driver to the requird teamcity directory. it just so happens it in the docker volume we can't extend the teamciry docker image with this change as files aren't copied to volumes

--- tc-docker-host/setup.sh
+++ tc-docker-host/setup.sh
@@ -3,6 +3,13 @@
 # create host directories for volumes
 sudo mkdir /home/bmantay/tc/data -p && mkdir /home/bmantay/tc/logs -p && mkdir /home/bmantay/tc/conf -p
 
+# create dir for db drivers
+sudo mkdir /home/bmantay/tc/data/lib/jdbc -p 
+
+# get the db drivers
+sudo apt-get install wget
+sudo wget https://github.com/Brianmantay/cicd-pipe/raw/master/tc-docker-host/sqljdbc42.jar -P /home/bmantay/tc/data/lib/jdbc/
+
 # run server container
 sudo docker run -d --name teamcity-server-instance  \
 	-v /home/bmantay/tc/data:/data/teamcity_server/datadir \
@@ -10,7 +17,7 @@ sudo docker run -d --name teamcity-server-instance  \
 	-p 8111:8111 \
 	jetbrains/teamcity-server
 
-# run agent container	
+# run agent container
 sudo docker run -d -e SERVER_URL="dockerhost-bmantay.eastus.cloudapp.azure.com:8111" \
 	--name teamcity-agent-instance \
 	-v /home/bmantay/tc/conf:/data/teamcity_agent/conf \
